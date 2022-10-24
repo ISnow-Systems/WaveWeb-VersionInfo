41368ee646ae28d14f0daabe4ea7030e778b3916
Hinata Hojo <hinata.hojo@isnow.xyz>
2022-10-24 16:39:55 +0900

    git 初期化

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <v-btn icon @click.stop="">
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'DefaultLayout',
+  data () {
+    return {
+      title: 'Version Information',
+    }
+  },
+
+})
+</script>
